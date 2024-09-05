793e377b567cdac6f12815e903e826829610fe2d
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-09-05 10:31:52 +0300

    update changes

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  const handleclick = () => {
+    setMessage('Good Morning Devs');
+  }
+ return (
+  <div>
+    <button onClick={handleclick}>
+      click me 
+    </button>
+    <p>{message}</p>
+  </div>
+ );
+}
+export default App;
